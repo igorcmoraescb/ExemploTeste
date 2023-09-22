b8b4d42e814c50f546c9d0c92a690c28a0f37503
Tragedia <ti.igor.m@gmail.com>
2023-09-22 20:11:20 -0300

    Atividade de Teste

--- src/codigos/Calculadora.java
+++ src/codigos/Calculadora.java
@@ -1,2 +1,12 @@
-package codigos;public class Calculadora {
+/*Aqui eu só coloquei a função de soma da calculadora. A esta altura, no curso,
+vocês devem ser capazes de a partir de um exemplo simples, adquirirem de forma
+independente a resolução de enunciados mais complexos.
+ */
+
+package codigos;
+
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a+b;
+    }
 }
--- src/testes/CalculadoraTest.java
+++ src/testes/CalculadoraTest.java
@@ -1,2 +1,33 @@
-package testes;public class CalculadoraTest {
+/*Aqui eu só coloquei dois testes e obviamente só da função de soma da calculadora, porque só
+ela foi implementada na classe Calculadora. A esta altura, no curso,
+vocês devem ser capazes de a partir de um exemplo simples, adquirirem de forma
+independente a resolução de enunciados mais complexos.
+ */
+
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import codigos.Calculadora;
+
+class CalculadoraTest {
+
+    @Test
+    void testSomar1() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.somar(2, 2);
+        int esperado = 4;
+        assertTrue(resultado == esperado);
+    }
+
+    @Test
+    void testSomar2() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.somar(3, 5);
+        int esperado = 8;
+        assertTrue(resultado == esperado);
+    }
+
 }
